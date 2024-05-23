644901351e10f1d0efce99dce464d39eb6371bf3
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-05-23 17:40:55 -0300

    Retorno de metodos

---
+++ Models/Curso.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp_manipulando_dados_dio.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AddAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
+    }
+}
