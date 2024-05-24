afccd601921dfbe1925bb1d7611f2f2f78675a78
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-05-24 10:01:49 -0300

    Formatando valores monetarios

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using csharp_manipulando_dados_dio.Models;
 
-Pessoa p1 = new Pessoa(nome: "Lucas", sobrenome: "Seixas");
 
-Pessoa p2 = new Pessoa(sobrenome: "Parker", nome: "Thabata");
+//Formatação de valores monetarios
+decimal ValorMonetario = 5.01M;
+Console.WriteLine($"{ValorMonetario:c}");
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AddAluno(p1);
-cursoDeIngles.AddAluno(p2);
-cursoDeIngles.ListarAlunos();
+
+//Aulas anteriores
+// Pessoa p1 = new Pessoa(nome: "Lucas", sobrenome: "Seixas");
+
+// Pessoa p2 = new Pessoa(sobrenome: "Parker", nome: "Thabata");
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AddAluno(p1);
+// cursoDeIngles.AddAluno(p2);
+// cursoDeIngles.ListarAlunos();
